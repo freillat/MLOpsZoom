f05ee05807100a7fa2c739f47bdcda6a81ba09cc
freillat <96790464+freillat@users.noreply.github.com>
2025-05-21 17:06:06 +0000

    q6 hw2

--- 02-experiment-tracking/register_model.py
+++ 02-experiment-tracking/register_model.py
@@ -71,10 +71,17 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_ids=experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=top_n,
+        order_by=["metrics.test_rmse ASC"]
+    )[0]
+    run_id = best_run.info.run_id
+    model_uri = f"runs:/{run_id}/model"
 
     # Register the best model
-    # mlflow.register_model( ... )
+    mlflow.register_model(model_uri, "best model")
 
 
 if __name__ == '__main__':
